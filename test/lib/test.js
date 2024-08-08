01ca777103513d23839ca0d051646e5d10bbcd1e
t-ski <t.schiepanski@gmx.de>
2024-08-08 22:18:49 +0200

    Add function to obtain a space value with SCSS

--- test/lib/test.js
+++ test/lib/test.js
@@ -7,7 +7,7 @@ const flecss = require("../../lib/api");
 const TRANSPILED = flecss.transpile(join(__dirname, "./_test.scss"));
 
 deepEqual(TRANSPILED.loadedUrls.length, 1);
-deepEqual(TRANSPILED.coreCSS.length > 100, true);
-deepEqual(TRANSPILED.sourceCSS, "a{color:green}a{color:red}");
+deepEqual(TRANSPILED.css.length > 100, true);
+deepEqual(TRANSPILED.css.includes("a{color:green}a{color:red}"), true);
 
 console.log("\x1b[32mLibrary test succeeded.\x1b[0m");

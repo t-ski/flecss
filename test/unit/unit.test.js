db89844b88d5232b3b480e5811d6421a77466a41
t-ski <t.schiepanski@gmx.de>
2024-11-13 13:26:16 +0100

    Introduce global @use identifier 'flecss' for access to utilities

--- test/unit/unit.test.js
+++ test/unit/unit.test.js
@@ -4,30 +4,27 @@ const { join } = require("path");
 const flecss = require("../../lib/api");
 
 
-const SCSS_FILE_PATH = join(__dirname, "./_test.scss");
-
-
 const transpiled = flecss
     .createTranspiler({
         development: true
     })
-    .fromFile(SCSS_FILE_PATH);
+    .fromFile(join(__dirname, "./flecss/_test.scss"));
 
-deepEqual(transpiled.loadedUrls.length, 26);
+deepEqual(transpiled.loadedUrls.length, 27);
 deepEqual(transpiled.css.length > 100, true);
 deepEqual(/a\s*\{\s*color:\s*red\s*[;}]/.test(transpiled.css), true);
-deepEqual(/a\s*\{\s*color:\s*green\s*[;}]/.test(transpiled.css), true);
+deepEqual(/a\s*\{\s*color:\s*var\(--flecss_color--lime--normal\)\s*[;}]/.test(transpiled.css), true);
 
 const transpiledStandalone = flecss
     .createTranspiler({
         library: "standalone"
     })
-    .fromFile(SCSS_FILE_PATH);
+    .fromFile(join(__dirname, "./standalone/_test.scss"));
 
 deepEqual(transpiledStandalone.loadedUrls.length, 1);
 deepEqual(transpiledStandalone.css.length < 100, true);
 deepEqual(/a\s*\{\s*color:\s*red\s*[;}]/.test(transpiledStandalone.css), true);
-deepEqual(/a\s*\{\s*color:\s*green\s*[;}]/.test(transpiledStandalone.css), false);
+deepEqual(/a\s*\{\s*color:\s*lime\s*[;}]/.test(transpiledStandalone.css), false);
 
 
 console.log("\x1b[32mUnit tests succeeded.\x1b[0m");

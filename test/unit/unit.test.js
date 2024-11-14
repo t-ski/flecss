72bb3de5d368ba4cfdc135ff07179fbfda31fe85
t-ski <t.schiepanski@gmx.de>
2024-11-14 13:16:30 +0100

    Reactivate only breakpoint variable CSS properties

--- test/unit/unit.test.js
+++ test/unit/unit.test.js
@@ -10,7 +10,7 @@ const transpiled = flecss
     })
     .fromFile(join(__dirname, "./flecss/_test.scss"));
 
-deepEqual(transpiled.loadedUrls.length, 27);
+deepEqual(transpiled.loadedUrls.length, 29);
 deepEqual(transpiled.css.length > 100, true);
 deepEqual(/a\s*\{\s*color:\s*red\s*[;}]/.test(transpiled.css), true);
 deepEqual(/a\s*\{\s*color:\s*var\(--flecss_color--lime--normal\)\s*[;}]/.test(transpiled.css), true);
